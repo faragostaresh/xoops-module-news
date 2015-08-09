f1fcfd1bfcceb5037154fa3af0c341a92e2d94eb
voltan <djvoltan@gmail.com>
2015-08-09 10:43:10 +0430

    Update module

--- news/blocks/breaking.php
+++ news/blocks/breaking.php
@@ -1,13 +1,11 @@
 <?php
 
 function news_breaking_show($options) {
+    $story_handler = xoops_getmodulehandler('story', 'news');
+    $story = $story_handler->News_StoryBreaking();
     $block = array();
-
-    $block['contents'] = array();
-
+    $block['contents'] = $story;
     return $block;
 }
 
-function news_breaking_edit($options) {
-
-}
+function news_breaking_edit($options) {}
